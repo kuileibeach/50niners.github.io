cec55457bbfce80e8b31f987657fe5bdfcde229e
kuileibeach <47800756+kuileibeach@users.noreply.github.com>
2019-04-10 14:30:18 -1000

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'kbg-icon4.png') {
+    if(mySrc === 'kicking-ball.png') {
       myImage.setAttribute ('src','sunrise-dh.png');
     } else {
-      myImage.setAttribute ('src','kbg-icon4.png');
+      myImage.setAttribute ('src','kicking-ball.png');
     }
 }
 
